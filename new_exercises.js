718001f840166eee71824957538c253fb9add80b
David Mendes <david.mendes@mindswap.academy>
2022-10-10 09:55:22 +0100

    first commit

---
+++ new_exercises.js
@@ -0,0 +1,112 @@
+/**
+ * Set of exercises to check if a person is legal age
+ */
+
+const LEGAL_AGE = 18;
+const people = [
+    {
+        name: 'John',
+        age: 22
+    },
+    {
+        name: 'Jane',
+        age: 18
+    },
+    {
+        name: 'Bob',
+        age: 16
+    },
+    {
+        name: 'Scarlett',
+        age: 24
+    },
+    {
+        name: 'Bruce',
+        age: 15
+    },
+    {
+        name: 'Angelina',
+        age: 19
+    }
+];
+
+/**
+ * Determines if all ages are legal
+ * @param {object[]} people
+ * @returns {boolean} - true if all ages are legal; false otherwise
+ */
+const allLegalAge = people => {
+
+};
+
+/**
+ * Determines if at least one person is of legal age
+ * @param {object[]} people - the people
+ * @returns {boolean} - true if at least one age is legal; false otherwise
+ */
+const atLeastOneLegalAge = people => {
+
+};
+
+/**
+ * Returns the oldest person
+ * @param {object[]} people - the people
+ * @returns {object} - the oldest person
+ */
+const getOldestPerson = people => {
+
+};
+
+/**
+ * Returns an array of people of legal age
+ * @param {object[]} people - the people
+ * @returns {object[]} - the people of legal age
+ */
+const getLegalAgePeople = people => {
+
+};
+
+/**
+ * Returns an object containing an array of underage and another array of legal ages
+ * I. e. {underage: [16, 15], legal: [22, 18, 24, 19]}
+ * @param {object[]} people - the people
+ */
+const splitAges = people => {
+
+};
+
+/**
+ * Returns the first person of legal age
+ * @param {object[]} people - the people
+ * @return {object} - the first person of legal age
+ */
+const getFirstLegalAgePerson = people => {
+
+};
+
+/**
+ * Returns the people's names
+ * @param {object[]} people - the people
+ * @return {string[]} - the people's names
+ */
+const getNames = people => {
+
+};
+
+/**
+ * Returns the sum of all ages
+ * @param {object[]} people - the people
+ * @return {number} - the sum of all ages
+ */
+const getAgeSum = people => {
+
+};
+
+/**
+ * Returns an array of the names of people of legal age
+ * @param {object[]} people - the people
+ * @return {string[]} - the people's names
+ */
+const getLegalAgePeopleNames = people => {
+
+};
